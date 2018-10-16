4677d511e77c40b2d23b1448c6a80d2cca7dbee4
MisterFahrenheit <homar425@gmail.com>
2018-10-16 09:31:20 +0200

    lab 2

--- WojtasikPrognozaPogody/ViewController.swift
+++ WojtasikPrognozaPogody/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var wind_direction: UITextField!
     @IBOutlet weak var air_pressure: UITextField!
     
-    let urlStr: String = "https://www.metaweather.com/api/location/44418/2013/4/27/"
+    let urlStr: String = "https://www.metaweather.com/api/location/"
     
     var resp: String = ""
     
@@ -40,15 +40,39 @@ class ViewController: UIViewController {
     }
     
     func loadWeather(){
-        let url: URL = URL.init(string: urlStr)!
+        let utlT: String = urlStr + getCity() + currentDate()
+        print(utlT)
+        let url: URL = URL.init(string: utlT)!
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
-            self.resp = String(data: data, encoding: .utf8)!
-            JSONSerialization.jsonObject(with: self.resp)
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {return}
+            DispatchQueue.main.async {
+                self.updateView(jsonArray: json!)
+            }
         }
-        
         task.resume()
     }
+    
+    func getCity() -> String{
+        return "44418/"
+    }
+    
+    func currentDate() -> String{
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        return "\(year)/\(month)/\(day)/"
+    }
+    
+    func updateView(jsonArray: [[String:Any]]){
+        self.max_temp.insertText("\(jsonArray[1]["max_temp"]!)")
+        self.min_temp.insertText("\(jsonArray[1]["min_temp"]!)")
+        self.wind_speed.insertText("\(jsonArray[1]["wind_speed"]!)")
+        self.wind_direction.insertText("\(jsonArray[1]["wind_direction"]!)")
+        self.air_pressure.insertText("\(jsonArray[1]["air_pressure"]!)")
+        }
 
     @IBAction func displayNextDay(_ sender: UIButton) {
     }
