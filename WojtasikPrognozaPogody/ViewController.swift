edeac0449e7a9f564528b4f1db16740fd240cde0
PW <prw@PWs-iMac.local>
2018-10-27 22:59:18 +0200

    only img to add

--- WojtasikPrognozaPogody/ViewController.swift
+++ WojtasikPrognozaPogody/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, WeatherData {
+
+    
 
     @IBOutlet weak var date: UILabel!
-    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet var imgView: UIImageView!
+    
+    
     @IBOutlet weak var min_temp: UITextField!
     @IBOutlet weak var max_temp: UITextField!
     @IBOutlet weak var wind_speed: UITextField!
@@ -21,91 +25,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var nextBtn: UIButton!
     @IBOutlet weak var previousBtn: UIButton!
     @IBOutlet weak var currentLocalizationLabel: UILabel!
+    @IBOutlet weak var theTemp: UITextField!
+    @IBOutlet weak var humidity: UITextField!
+    @IBOutlet weak var visibility: UITextField!
+    @IBOutlet weak var predictability: UITextField!
     
-    let locationManager = CLLocationManager()
+//    let locationManager = CLLocationManager()
     
     
-    let urlStr: String = "https://www.metaweather.com/api/location/"
-    var weatherData: [[String: Any]] = []
+    var client: WeatherClient!
+    
+    var weatherData: [DayWeatherForecast]?
+    var weatherImg: UIImage?
     var choosenDayId: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initLocation()
-        loadWeather()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.client = WeatherClient(owner: self)
+        self.client.getCityWeather(cityCode: "44418/")
     }
     
-    
-    func initLocation(){
-        self.locationManager.requestAlwaysAuthorization()
-        self.locationManager.delegate = self
-        self.locationManager.startMonitoringSignificantLocationChanges()
-    }
-    
-    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
-        CLGeocoder().reverseGeocodeLocation(locations.last!, completionHandler: {(placemarks, error) -> Void in
-            
-            if error != nil {
-                print("Reverse geocoder failed with error" + error!.localizedDescription)
-                return
-            }
-            
-            if placemarks!.count > 0 {
-                print(placemarks!)
-            }
-            else {
-                print("Problem with the data received from geocoder")
-            }
-        })
-        
-        // Do something with the location.
-    }
-    
-    func loadWeather(){
-        let utlT: String = urlStr + getCity()
-        print(utlT)
-        let url: URL = URL.init(string: utlT)!
-        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {return}
-            guard let weatherData = json!["consolidated_weather"] as? [[String: Any]] else {return}
-            DispatchQueue.main.async {
-                self.weatherData = weatherData
-                self.updateView()
-            }
-        }
-        task.resume()
-    }
-    
-    func findCity(){
-        
-    }
-    
-    func getCity() -> String{
-        return "44418/"
-    }
-    
-    func currentDate() -> String{
-        let date = Date()
-        let calendar = Calendar.current
-        let year = calendar.component(.year, from: date)
-        let month = calendar.component(.month, from: date)
-        let day = calendar.component(.day, from: date)
-        return "\(year)/\(month)/\(day)/"
-    }
-    
-    func updateView(){
-        self.max_temp.text = prepareTxt(field: "max_temp")
-        self.min_temp.text = prepareTxt(field: "min_temp")
-        self.wind_speed.text = prepareTxt(field: "wind_speed")
-        self.wind_direction.text = prepareTxt(field: "wind_direction")
-        self.air_pressure.text = prepareTxt(field: "air_pressure")
-        self.date.text = prepareTxt(field: "applicable_date")
+    func updateDayView(){
+        let day = weatherData![self.choosenDayId]
+        self.client.getWeatherStateIcon(abbreviation: day.weatherStateAbbr)
+        self.visibility.text = format(f: day.visibility)
+        self.predictability.text = String(day.predictability)
+        self.humidity.text = String(day.humidity)
+        self.theTemp.text = format(f: day.theTemp)
+        self.max_temp.text = format(f: day.maxTemp)
+        self.min_temp.text = format(f: day.minTemp)
+        self.wind_speed.text = format(f: day.windSpeed) + "m/s"
+        self.wind_direction.text = format(f: day.windDirection)
+        self.air_pressure.text = format(f: day.airPressure)
+        self.date.text = String(day.applicableDate)
         }
     
-    func prepareTxt(field: String) -> String{
-        return "\(self.weatherData[self.choosenDayId][field]!)"
+    func format(f: Double) -> String{
+        return String(format: "%.1f", f)
+    }
+    
+    func updateWeatherImg(img: UIImage){
+        self.imgView = UIImageView(image: img)
     }
     
     func updateChoosenDayId(newId: Int) -> Bool {
@@ -120,7 +80,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBAction func nextBtnClick(_ sender: Any) {
         if (updateChoosenDayId(newId: self.choosenDayId+1)){
-            self.updateView()
+            self.updateDayView()
             previousBtn.isEnabled = true
         }
         else{
@@ -131,7 +91,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBAction func previousBtnClick(_ sender: Any) {
         if(updateChoosenDayId(newId: self.choosenDayId-1)){
-        self.updateView()
+            self.updateDayView()
             nextBtn.isEnabled = true
         }
         else{
---
+++ WojtasikPrognozaPogody/WeatherClient.swift
@@ -0,0 +1,96 @@
+//
+//  ForecastData.swift
+//  WojtasikPrognozaPogody
+//
+//  Created by PW on 23/10/2018.
+//  Copyright © 2018 RS KIS.EDU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CityWeatherResponse: Codable {
+    let consolidatedWeather: [DayWeatherForecast]
+
+    enum CodingKeys: String, CodingKey {
+        case consolidatedWeather = "consolidated_weather"
+    }
+}
+
+struct DayWeatherForecast: Codable {
+    let id: Int
+    let weatherStateName, weatherStateAbbr, windDirectionCompass, created: String
+    let applicableDate: String
+    let minTemp, maxTemp, theTemp, windSpeed: Double
+    let windDirection, airPressure: Double
+    let humidity: Int
+    let visibility: Double
+    let predictability: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case weatherStateName = "weather_state_name"
+        case weatherStateAbbr = "weather_state_abbr"
+        case windDirectionCompass = "wind_direction_compass"
+        case created
+        case applicableDate = "applicable_date"
+        case minTemp = "min_temp"
+        case maxTemp = "max_temp"
+        case theTemp = "the_temp"
+        case windSpeed = "wind_speed"
+        case windDirection = "wind_direction"
+        case airPressure = "air_pressure"
+        case humidity, visibility, predictability
+    }
+}
+
+protocol WeatherData{
+    var weatherData: [DayWeatherForecast]? {get set}
+    func updateDayView()
+    func updateWeatherImg(img: UIImage)
+}
+
+class WeatherClient{
+    
+    let apiUrl: String = "https://www.metaweather.com/api/"
+    let staticUrl: String = "https://www.metaweather.com/static/"
+    var weatherForecast: [DayWeatherForecast]?
+    var owner: WeatherData
+    
+    init(owner: WeatherData) {
+        self.owner = owner
+    }
+    
+    func getCityWeather(cityCode: String){
+        print(apiUrl + "location/" + cityCode)
+        let url: URL = URL.init(string: apiUrl + "location/" + cityCode)!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            do{
+            let responseObj = try JSONDecoder().decode(CityWeatherResponse.self, from: data)
+                
+                DispatchQueue.main.async{
+                    self.owner.weatherData = responseObj.consolidatedWeather
+                    self.owner.updateDayView()
+                }
+            }
+            catch{
+                print("\(error)")
+            }
+        }
+        task.resume()
+    }
+    
+    func getWeatherStateIcon(abbreviation: String){
+        let url: URL = URL.init(string: staticUrl + "img/weather/png/\(abbreviation).png")!
+        print(url)
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {return}
+            DispatchQueue.main.async{
+                let img = UIImage(data: data)
+                self.owner.updateWeatherImg(img: img!)
+            }
+        }
+        task.resume()
+    }
+}
